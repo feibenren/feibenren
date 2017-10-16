bff51e01c29fa1ad73017a06c0e087dc42c76ca9
feibenren <1057808091@qq.com>
2017-10-16 16:21:05 +0800

    asdf

--- test.js
+++ test.js
@@ -1,6 +1,13 @@
-if({}){
-    console.log('true');
-}else{
-    console.log('false');
+try{
+    console.log(111);
+    setTimeout(function() {
+        throw new Error('setTimeout err');  
+    }, 2000);
+}catch(e){
+    console.log('catch:');
+    console.log(e);
+}finally{
+    console.log('finally');
 }
 
+console.log('after try');
