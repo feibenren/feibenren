e3556a078d7c6fc2aaa14b8f3a12fd28496bb3d1
feibenren <1057808091@qq.com>
2017-11-02 14:42:54 +0800

    asdf

--- test.js
+++ test.js
@@ -1,22 +1,4 @@
-var words = ['你好', '在吗', '欢迎', '请问您有什么需要'];
-var i = 0;
-
-function say(t) {
-    setTimeout(function() {
-        console.log(words[i]);
-        i++;
-    }, t);
-}
-
-var g = function*() {
-    yield say(1000);
-    yield say(2000);
-    yield say(3000);
-    yield say(4000);
-}
-
-var res = g();
-res.next();
-res.next();
-res.next();
-res.next();
+var today=new Date();
+console.log(today.toDateString())
+console.log(today.toTimeString())
+console.log(today.toString())
